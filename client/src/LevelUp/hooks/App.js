adfc3b1788eb4cc968b99219b917433aa7067dea
Michael <checomichael2@gmail.com>
2019-03-16 12:57:15 -0400

    useMemo

--- client/src/LevelUp/hooks/App.js
+++ client/src/LevelUp/hooks/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React, { useState, useEffect, createContext, useMemo } from 'react';
 import ReFactor, { Toggler } from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 export const UserContext = createContext();
 const App = () => {
 	const [name, setName] = useTitleInput('');
+	const reverseWord = word => {
+		console.log('function called');
+		return word
+			.split('')
+			.reverse()
+			.join('');
+	};
+	const title = 'Level Up Dishes';
+	const TitleReversed = useMemo(() => reverseWord(title), [title]);
 	return (
 		<UserContext.Provider
 			value={{
