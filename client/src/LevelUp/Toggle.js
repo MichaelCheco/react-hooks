8160c3644d9e36a10b06a4d234143631d8ccd6c9
Michael <checomichael2@gmail.com>
2019-03-16 12:34:54 -0400

    useContext

--- client/src/LevelUp/Toggle.js
+++ client/src/LevelUp/Toggle.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useContext } from 'react';
+import { UserContext } from './hooks/App';
 
 export default class ReFactor extends Component {
 	state = {
@@ -21,14 +22,16 @@ export default class ReFactor extends Component {
 
 export const Toggler = () => {
 	const [isToggled, setIsToggled] = useState(false);
-
+	const { user } = useContext(UserContext);
 	// const toggle = () => {
 	// 	setIsToggled(!isToggled);
 	// };
 	return (
-		<div>
-			<button onClick={() => setIsToggled(!isToggled)}>Toggle hook</button>
-			<h2>{isToggled && <h2>Hello</h2>}</h2>
-		</div>
+		user && (
+			<div>
+				<button onClick={() => setIsToggled(!isToggled)}>Toggle hook</button>
+				<h2>{isToggled && <h2>Hello</h2>}</h2>
+			</div>
+		)
 	);
 };
--- client/src/LevelUp/hooks/App.js
+++ client/src/LevelUp/hooks/App.js
@@ -1,24 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import ReFactor, { Toggler } from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
+
+export const UserContext = createContext();
 const App = () => {
 	const [name, setName] = useTitleInput('');
 	return (
-		<div className="main-wrapper">
-			<h1>Level Up Dishes</h1>
-			<Toggler />
-			<form
-				onSubmit={e => {
-					e.preventDefault();
-				}}>
-				<input
-					type="text"
-					value={name}
-					onChange={e => setName(e.target.value)}
-				/>
-				<button type="submit" />
-			</form>
-		</div>
+		<UserContext.Provider
+			value={{
+				user: true,
+			}}>
+			<div className="main-wrapper">
+				<h1>Level Up Dishes</h1>
+				<Toggler />
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+					}}>
+					<input
+						type="text"
+						value={name}
+						onChange={e => setName(e.target.value)}
+					/>
+					<button type="submit" />
+				</form>
+			</div>
+		</UserContext.Provider>
 	);
 };
 
