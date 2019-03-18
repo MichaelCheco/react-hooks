87e46e2897bf99d3b82ca797ab407ad0d7b96b72
Michael <checomichael2@gmail.com>
2019-03-18 14:12:03 -0400

    remoev logs

--- client/src/Usage.js
+++ client/src/Usage.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { AppContext } from './useContext';
 function Usage() {
 	const { name } = useContext(AppContext);
-	console.log(name, 'U');
 	return <h1>{name}</h1>;
 }
 export default Usage;
--- client/src/useContext.js
+++ client/src/useContext.js
@@ -2,7 +2,6 @@ import React, { createContext } from 'react';
 import Usage from './Usage';
 
 export const AppContext = createContext();
-console.log(AppContext, 'A');
 const ContextApp = () => {
 	return (
 		<AppContext.Provider value={{ name: 'Michael' }}>
